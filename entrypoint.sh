0dc35b73dfee04572b9f3092cf4cc424b832ec4e
Vivek Lanjekar <vivek.lanjekar@gmail.com>
2018-08-24 18:34:41 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Tested using bash version 4.1.5
+for ((i=1;i<=100000000;i++));
+do
+   # your-unix-command-here
+   d=`date -Iseconds`
+   echo "$d $i ERROR something happened in this execution."
+done
