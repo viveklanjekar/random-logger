471e0a8151507c1b730bdacb2d1f3178ffac1582
Vivek Lanjekar <vivek.lanjekar@gmail.com>
2020-03-24 02:20:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,11 @@ do
    instruction=$(shuf -i 0-4 -n 1)
    d=`date -Iseconds`
    case "$instruction" in
-      "1") echo "$d ERROR vivek.lanjekar@gmail.com, vivek.lanjekar@hotmail in this execution."
+      "1") echo "ERROR vivek.lanjekar@gmail.com, vivek.lanjekar@hotmail in this execution."
       ;;
       "2") echo "$d INFO vivek.lanjekar@gmail.com, vivek.lanjekar@hotmail takes the value and converts it to string."
       ;;
-      "3") echo "$d WARN vivek.lanjekar@gmail.com variable not in use."
+      "3") echo "WARN vivek.lanjekar@gmail.com variable not in use."
       ;;
       "4") echo "$d DEBUG vivek.lanjekar@gmail.com, vivek.lanjekar@hotmail first loop completed."
       ;;
